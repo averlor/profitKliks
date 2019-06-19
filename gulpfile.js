6c15dd04029a35552732f51324d06d834cd3584d
Vladimir Abramyants <vabramanc@gmail.com>
2019-06-19 13:28:11 +0300

    Added file -> gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+global.$ = {
+    gulp: require('gulp'),
+    glp: require('gulp-load-plugins')(),
+    bs: require('browser-sync').create(),
+
+    path: {
+        tasks: require('./gulp/config/tasks.js')
+    }
+};
+
+$.path.tasks.forEach(taskPath => {
+    require(taskPath)()
+});
+
+// start gulp
+$.gulp.task('default', $.gulp.series(
+    // TODO: return 'scripts:lib'
+    $.gulp.parallel('pug', 'sass', 'scripts', 'img:dev'),
+    $.gulp.parallel('watch', 'serve')
+));
+
+// $.gulp.task('build', $.gulp.series(
+//     $.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts', 'img:build'),
+//     $.gulp.parallel('watch', 'serve')
+// ))
